d9b16c5f6642ac0d5edd92dcd371515e99e8fec6
travinanastia <nastiatravina777@gmail.com>
2023-03-30 10:05:01 +0300

    first test

---
+++ tests/test_functions.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import pytest
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+from main import count_words, popular_words
+
+@pytest.fixture
+def input_file():
+    input_file = "input.txt"
+    with open(input_file, "w") as f:
+        f.write("Hello, how are you? Are you ok? We shall go to the mall. We are at the mall. Are apples sweet?")
+    return input_file
+
+@pytest.fixture
+def output_file():
+    return "output.txt"
+
+def test_count_words(input_file):
+    words_counts = count_words(input_file)
+    expected = {'hello': 1, 'how': 1, 'are': 4, 'you': 2, 'ok': 1, 'we': 2, 'shall': 1, 'go': 1, 'to': 1, 'the': 2, 'mall': 2, 'at': 1, 'apples': 1, 'sweet': 1}
+    assert words_counts == expected
+
+
