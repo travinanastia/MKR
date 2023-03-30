b26fe4e74ed7c3227291fc785b9cfa2ffe3ac6a4
travinanastia <nastiatravina777@gmail.com>
2023-03-30 10:13:31 +0300

    firsts test for popular_words

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -22,4 +22,14 @@ def test_count_words(input_file):
     expected = {'hello': 1, 'how': 1, 'are': 4, 'you': 2, 'ok': 1, 'we': 2, 'shall': 1, 'go': 1, 'to': 1, 'the': 2, 'mall': 2, 'at': 1, 'apples': 1, 'sweet': 1}
     assert words_counts == expected
 
+def test_popular_words_prewritten(input_file, output_file):
+    popular_words(input_file, output_file)
+    result = []
+    with open(output_file, "r") as file:
+        for line in file:
+            word, count = line.strip().split("-")
+            result.append((word, int(count)))
+    expected = [('are', 4), ('you', 2),('we', 2),('the', 2),('mall', 2), ('hello', 1),('how', 1),('ok', 1),('shall', 1),('go', 1)]
+    assert result == expected
+
 
