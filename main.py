ca24f4c29b6da57cc421505bdc86cc1d3649a065
travinanastia <nastiatravina777@gmail.com>
2023-03-30 09:25:45 +0300

    count words functions

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ def open_file(input):
         text = file.read()
     return text
     
-def write_file(output_file, most_common_words):
-    with open(output_file, "w") as file:
-        for word, count in most_common_words:
+def write_file(output, popular_words):
+    with open(output, "w") as file:
+        for word, count in popular_words:
             file.write(f"{word}-{count}\n")
 
+def count_words(input):
+    text = open_file(input)
+    words = text.lower().replace("\n", " ").replace(",", " ").replace(".", " ").replace("!", " ").replace("?", " ").split()
+    words_counts = {}
+    for word in words:
+        if word not in words_counts:
+            words_counts[word] = 1
+        else:
+            words_counts[word] += 1
+    return words_counts
 
 
 
